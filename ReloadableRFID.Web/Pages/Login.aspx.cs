eb5df8c5109d67c2d343dda7797276417154af1f
Keen David Aguro <agurokeendavid@gmail.com>
2021-01-16 14:38:02 +0800

    change content of login page.

--- ReloadableRFID.Web/Pages/Login.aspx.cs
+++ ReloadableRFID.Web/Pages/Login.aspx.cs
@@ -4,14 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ReloadableRFID.Library.Models;
+using ReloadableRFID.Library.Repositories;
 
 namespace ReloadableRFID.Web.Pages
 {
     public partial class Login : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected async void Page_Load(object sender, EventArgs e)
         {
-
+            
+            if (IsPostBack)
+            {
+                var user = await UserRepository.GetUserDetails(txtUsername.Text, txtPassword.Text);
+                if (user != null)
+                {
+                    Session["UserId"] = user.Id;
+                    Session["Username"] = user.Username;
+                    Session["AccountTypeId"] = user.AccountTypeId;
+                    Response.Write(@"<script>alert('Successfully Logged in!');</script>");
+                    Response.Redirect("~/Pages/Home.aspx", false);
+                }
+                else
+                {
+                    Response.Write(@"<script>alert('Invalid username/password');</script>");
+                }
+            }
+            else
+            {
+                Session.Clear();
+                Session.Abandon();
+            }
         }
     }
 }
